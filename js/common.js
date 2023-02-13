772a6570955f1dc7a0b5736d6982a40b3b50186d
kimyoonjii <kimuj0823@naver.com>
2023-02-13 15:37:53 +0900

    티쏘 최초 업로드

---
+++ js/common.js
@@ -0,0 +1,16 @@
+'use strict';
+
+window.addEventListener('load', function() {
+  preventDefault();
+
+}, false);
+
+
+function preventDefault() {
+  const links = document.querySelectorAll('a[href="#"]');
+  links.forEach(function(element) {
+    element.addEventListener('click', function(e) {
+      e.preventDefault();
+    }, false);
+  });
+}
